856439abaf1ae7362983f92fd5087d582b6efa45
root <erezc15@gmail.com>
2018-01-11 14:21:45 +0200

    1. created a blacklist text file of viruses. 2. created a function that extracts the file name from a url. 3. we now look from the file name in the blacklist instead of doing it hard-coded. 4. TODO: figure out how to support https sites (the "CONNECT" saga).

--- mod_example2.c
+++ mod_example2.c
@@ -6,12 +6,13 @@
 
 #include "stdio.h"
 #include "stdlib.h"
+#include "string.h"
 
 
 /* Define prototypes of our functions in this module */
 static void register_hooks(apr_pool_t *pool);
 static int example2_handler(request_rec *r);
-
+static char* virusName(char* str);
 
 typedef struct {
     int         enabled;      /* Enable or disable our module */
@@ -21,9 +22,9 @@ typedef struct {
 
 static example_config config;
 
-static int example2_handler(request_rec *r)
+static int example2_handler(request_rec *r2)
 {
-    if (!r->handler || strcmp(r->handler, "example2")) return(DECLINED);
+    if (!r2->handler || strcmp(r2->handler, "example2")) return(DECLINED);
 
     // ap_set_content_type(r, "text/plain");
 
@@ -35,13 +36,15 @@ static int example2_handler(request_rec *r)
         // fprintf(r, "File Opened without errors\n\n");
         fprintf(fp, "A click:\n");
 
+        request_rec *r = ap_sub_req_lookup_uri(r2->uri,r2,NULL);
+
         fprintf(fp, "\tEnabled: %u\n", config.enabled);
         fprintf(fp, "\tPath: %s\n", config.path);
         fprintf(fp, "\tTypeOfAction: %x\n\n", config.typeOfAction);
 
-        // fprintf(fp, "\tap_get_server_name: %s\n", ap_get_server_name(r));
-        // fprintf(fp, "\tap_document_root: %s\n", ap_document_root(r));
-        // fprintf(fp, "\tap_get_server_name_for_url: %s\n", ap_get_server_name_for_url(r));
+        fprintf(fp, "\tap_get_server_name: %s\n", ap_get_server_name(r));
+        fprintf(fp, "\tap_document_root: %s\n", ap_document_root(r));
+        fprintf(fp, "\tap_get_server_name_for_url: %s\n", ap_get_server_name_for_url(r));
         fprintf(fp, "\tREQUEST RECORD STRUCT (PARTIAL):\n");                                // some fields skipped here
         // fprintf(fp, "\t\tCONNECTION STRUCT (PARTIAL):\n");                                    // some fields skipped here
         // fprintf(fp, "\t\t\tclient_ip: %s\n", r->connection->client_ip);
@@ -66,46 +69,46 @@ static int example2_handler(request_rec *r)
         // fprintf(fp, "\t\t\tmain->the request: %s\n", r->main->the_request);
 
         fprintf(fp, "\t\tthe_request: %s\n", r->the_request);
-        // fprintf(fp, "\t\tassbackwards: %d\n", r->assbackwards);
+        fprintf(fp, "\t\tassbackwards: %d\n", r->assbackwards);
         fprintf(fp, "\t\tproxyreq: %d\n", r->proxyreq);
-        // fprintf(fp, "\t\theader_only: %d\n", r->header_only);
-        // fprintf(fp, "\t\tproto_num: %d\n", r->proto_num);
+        fprintf(fp, "\t\theader_only: %d\n", r->header_only);
+        fprintf(fp, "\t\tproto_num: %d\n", r->proto_num);
         fprintf(fp, "\t\tprotocol: %s\n", r->protocol);
         fprintf(fp, "\t\thostname: %s\n", r->hostname);
         fprintf(fp, "\t\trequest_time: %ld\n", r->request_time);
-        // fprintf(fp, "\t\tstatus_line: %s\n", r->status_line);
+        fprintf(fp, "\t\tstatus_line: %s\n", r->status_line);
         fprintf(fp, "\t\tstatus: %d\n", r->status);
-        // fprintf(fp, "\t\tmethod_number %d\n", r->method_number);
+        fprintf(fp, "\t\tmethod_number %d\n", r->method_number);
         fprintf(fp, "\t\tmethod: %s\n", r->method);
-        // fprintf(fp, "\t\tallowed: %ld\n", r->allowed);                                       // some fields skipped here
-        // fprintf(fp, "\t\tsent_bodyct: %ld\n", r->sent_bodyct);
-        // fprintf(fp, "\t\tbytes_sent: %ld\n", r->bytes_sent);
-        // fprintf(fp, "\t\tmtime: %ld\n", r->mtime);
-        // fprintf(fp, "\t\trange: %s\n", r->range);
-        // fprintf(fp, "\t\tclength: %ld\n", r->clength);
-        // fprintf(fp, "\t\tchunked: %d\n", r->chunked);
-        // fprintf(fp, "\t\tread_body: %d\n", r->read_body);
-        // fprintf(fp, "\t\tread_chunked: %d\n", r->read_chunked);
-        // fprintf(fp, "\t\texpecting_100: %u\n", r->expecting_100);                           // some fields skipped here
-        // fprintf(fp, "\t\tremaining: %ld\n", r->remaining);
-        // fprintf(fp, "\t\tread_length: %ld\n", r->read_length);                              // some fields skipped here
+        fprintf(fp, "\t\tallowed: %ld\n", r->allowed);                                       // some fields skipped here
+        fprintf(fp, "\t\tsent_bodyct: %ld\n", r->sent_bodyct);
+        fprintf(fp, "\t\tbytes_sent: %ld\n", r->bytes_sent);
+        fprintf(fp, "\t\tmtime: %ld\n", r->mtime);
+        fprintf(fp, "\t\trange: %s\n", r->range);
+        fprintf(fp, "\t\tclength: %ld\n", r->clength);
+        fprintf(fp, "\t\tchunked: %d\n", r->chunked);
+        fprintf(fp, "\t\tread_body: %d\n", r->read_body);
+        fprintf(fp, "\t\tread_chunked: %d\n", r->read_chunked);
+        fprintf(fp, "\t\texpecting_100: %u\n", r->expecting_100);                           // some fields skipped here
+        fprintf(fp, "\t\tremaining: %ld\n", r->remaining);
+        fprintf(fp, "\t\tread_length: %ld\n", r->read_length);                              // some fields skipped here
         fprintf(fp, "\t\tcontent_type: %s\n", r->content_type);
         fprintf(fp, "\t\thandler: %s\n", r->handler);
-        // fprintf(fp, "\t\tcontent_encoding: %s\n", r->content_encoding);                     // some fields skipped here
-        // fprintf(fp, "\t\tvlist_validator: %s\n", r->vlist_validator);
-        // fprintf(fp, "\t\tuser: %s\n", r->user);
-        // fprintf(fp, "\t\tap_auth_type: %s\n", r->ap_auth_type);
+        fprintf(fp, "\t\tcontent_encoding: %s\n", r->content_encoding);                     // some fields skipped here
+        fprintf(fp, "\t\tvlist_validator: %s\n", r->vlist_validator);
+        fprintf(fp, "\t\tuser: %s\n", r->user);
+        fprintf(fp, "\t\tap_auth_type: %s\n", r->ap_auth_type);
         fprintf(fp, "\t\tunparsed_uri: %s\n", r->unparsed_uri);
         fprintf(fp, "\t\turi: %s\n", r->uri);
         fprintf(fp, "\t\tfilename: %s\n", r->filename);
-        // fprintf(fp, "\t\tcanonical_filename: %s\n", r->canonical_filename);
-        // fprintf(fp, "\t\tpath_info: %s\n", r->path_info);
-        // fprintf(fp, "\t\targs: %s\n", r->args);
-        // fprintf(fp, "\t\tused_path_info: %d\n", r->used_path_info);
-        // fprintf(fp, "\t\teos_sent: %d\n", r->eos_sent);                                     // some fields skipped here
-        // fprintf(fp, "\t\tlog_id: %s\n", r->log_id);                                         // some fields skipped here
-        // fprintf(fp, "\t\tno_cache: %d\n", r->no_cache);
-        // fprintf(fp, "\t\tno_local_copy: %d\n", r->no_local_copy);                           // some fields skipped here
+        fprintf(fp, "\t\tcanonical_filename: %s\n", r->canonical_filename);
+        fprintf(fp, "\t\tpath_info: %s\n", r->path_info);
+        fprintf(fp, "\t\targs: %s\n", r->args);
+        fprintf(fp, "\t\tused_path_info: %d\n", r->used_path_info);
+        fprintf(fp, "\t\teos_sent: %d\n", r->eos_sent);                                     // some fields skipped here
+        fprintf(fp, "\t\tlog_id: %s\n", r->log_id);                                         // some fields skipped here
+        fprintf(fp, "\t\tno_cache: %d\n", r->no_cache);
+        fprintf(fp, "\t\tno_local_copy: %d\n", r->no_local_copy);                           // some fields skipped here
         
         fprintf(fp, "\t\tPARSED URI STRUCT (PARTIAL):\n");
         // fprintf(fp, "\t\t\tparsed_uri -> scheme: %s\n", r->parsed_uri.scheme);
@@ -123,12 +126,31 @@ static int example2_handler(request_rec *r)
         // fprintf(fp, "\t\t\tuseragent_addr -> servname: %s\n", r->useragent_addr->servname);
         
         fprintf(fp, "\t\tuseragent_ip: %s\n", r->useragent_ip);
+
         fclose(fp);
     }
 
-    if(strcmp(r->uri, "http://www.softwareclones.org/download/cyclone-0.1.3.tar.bz2") == 0) {
-        ap_set_content_type(r, "text/plain");
-        ap_rprintf(r, "No downloads for gopnik!");
+    FILE *BlackList;
+    BlackList = fopen("/var/www/html/BlackList.txt", "r+");
+
+    fseek(BlackList, 0, SEEK_END);
+    long fsize = ftell(BlackList);
+    fseek(BlackList, 0, SEEK_SET);  //same as rewind(f);
+
+    char *virusList = malloc(fsize + 1);
+    size_t numberOfItemsRead = fread(virusList, fsize, 1, BlackList);
+    fclose(BlackList);
+
+    FILE *virusTest;
+    virusTest = fopen("/var/www/html/virusTest.txt", "a+");
+
+    char* virus = virusName(r2->uri);
+    fprintf(virusTest, "\t\tvirus: %s\n", virus);
+    fclose(virusTest);
+
+    if(strstr(virusList, virusName(r2->uri)) != NULL) {
+        ap_set_content_type(r2, "text/plain");
+        ap_rprintf(r2, "No downloads for gopnik!");
         return (DONE);
     } else {
         return (DECLINED);
@@ -155,3 +177,30 @@ module AP_MODULE_DECLARE_DATA   example2_module =
     NULL,            /* Any directives we may have for httpd */
     register_hooks   /* Our hook registering function */
 };
+
+
+static char* virusName(char* str){
+
+    char *temp = malloc(strlen(str));
+    strcpy(temp, str);
+
+    int slashCounter = 0;
+
+    for (int i=0; i<strlen(temp); i++){
+            if(temp[i] == '/'){
+                slashCounter++;
+            }
+        }
+
+    int i = 0;
+    char *strTokens  = strtok(temp, "/");
+    char *tokensArray[slashCounter+1];
+
+    while(strTokens != NULL){
+        tokensArray[i++] = strTokens;
+        strTokens = strtok(NULL,  "/");
+    }
+
+    return tokensArray[slashCounter-1];
+}
+
