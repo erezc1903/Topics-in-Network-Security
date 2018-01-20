a9481cf5286b4c0ef9c8092bb55cd48192a60339
Cannonball123 <avinat123@gmail.com>
2018-01-20 12:16:57 +0200

    DONE: 1. Removed the config struct - not needed. 2. Added contracts for the functions.    - Up to this point, the source code works fine. TODO: 1. Test the module in a fresh environment. 2. Update names from mod_example to mod_virus_block 3. Write installation notes. 4. Write installation script. 5. Write project pdf.

--- mod_example2.c
+++ mod_example2.c
@@ -9,6 +9,7 @@
 #include "string.h"
 
 
+
 /* Define prototypes of our functions in this module */
 static void register_hooks(apr_pool_t *pool);
 static int example2_handler(request_rec *r);
@@ -17,69 +18,44 @@ static char* initVirusList();
 
 
 
-
-typedef struct {
-    int         enabled;      /* Enable or disable our module */
-    const char *path;         /* Some path to...something */
-    int         typeOfAction; /* 1 means action A, 2 means action B and so on */
-} example_config;
-
-
-
-
-static example_config config;
+/* Define the virus list */
 static char *virusList;
 
 
 
-
+/* Our handler function. receives a request record r, handles it. */
 static int example2_handler(request_rec *r)
 {
     if (!r->handler || strcmp(r->handler, "example2")) return(DECLINED);
-
-    // ap_set_content_type(r, "text/plain");
-
     FILE *fp;
     fp = fopen("/var/www/html/testFile.txt", "a+");
     if(fp == NULL) {
-        // fprintf(r, "Error opening file: %s\n\n", strerror(errno));
+        // do nothing
     } else {
-        // fprintf(r, "File Opened without errors\n\n");
         fprintf(fp, "A click:\n");
-        fprintf(fp, "\tREQUEST RECORD STRUCT (PARTIAL):\n");                                // some fields skipped here
-        
+        fprintf(fp, "\tREQUEST RECORD STRUCT (PARTIAL):\n");
         fprintf(fp, "\t\tthe_request: %s\n", r->the_request);
-        
         fprintf(fp, "\t\tprotocol: %s\n", r->protocol);
         fprintf(fp, "\t\thostname: %s\n", r->hostname);
         fprintf(fp, "\t\trequest_time: %ld\n", r->request_time);
-        
         fprintf(fp, "\t\tstatus: %d\n", r->status);
-        
         fprintf(fp, "\t\tmethod: %s\n", r->method);
-        
         fprintf(fp, "\t\tcontent_type: %s\n", r->content_type);
         fprintf(fp, "\t\thandler: %s\n", r->handler);
-        
         fprintf(fp, "\t\tunparsed_uri: %s\n", r->unparsed_uri);
         fprintf(fp, "\t\turi: %s\n", r->uri);
         fprintf(fp, "\t\tfilename: %s\n", r->filename);
-        
-        
         fprintf(fp, "\t\tuseragent_ip: %s\n", r->useragent_ip);
-
-        
     }
 
-    
     /* For debugging purposes */
-    FILE *virusTest;
-    virusTest = fopen("/var/www/html/virusTest.txt", "a+");
+    // FILE *virusTest;
+    // virusTest = fopen("/var/www/html/virusTest.txt", "a+");
 
-    char* virus = virusName(r->uri);
-    fprintf(virusTest, "\t\tvirus: %s\n", virus);
-    fprintf(virusTest, "\t\turi: %s\n\n", r->uri);
-    fclose(virusTest);
+    // char* virus = virusName(r->uri);
+    // fprintf(virusTest, "\t\tvirus: %s\n", virus);
+    // fprintf(virusTest, "\t\turi: %s\n\n", r->uri);
+    // fclose(virusTest);
 
     if( (virusName(r->uri) != NULL) && (strstr(virusList, virusName(r->uri)) != NULL)) {
         ap_set_content_type(r, "text/html");
@@ -96,16 +72,12 @@ static int example2_handler(request_rec *r)
 
 
 
-
+/* The function that registers our handler */
 static void register_hooks(apr_pool_t *pool) 
 {
-    config.enabled = 1;
-    config.path = "/foo/bar";
-    config.typeOfAction = 0x00;
     virusList = initVirusList();
     ap_hook_handler(example2_handler, NULL, NULL, APR_HOOK_REALLY_FIRST);
 }
-
 
 
 
@@ -123,14 +95,12 @@ module AP_MODULE_DECLARE_DATA   example2_module =
 
 
 
-
-
-
+/* This function receives a url and extracts the file name from it, if any.
+   If there isnt any file, returns NULL */
 static char* virusName(char* str){
 
     char *temp = malloc(strlen(str));
     strcpy(temp, str);
-
     int slashCounter = 0;
 
     for (int i=0; i<strlen(temp); i++){
@@ -153,9 +123,7 @@ static char* virusName(char* str){
 
 
 
-
-
-
+/* Initializes the virus list from a file */
 static char* initVirusList(){
 
     FILE *BlackList;
